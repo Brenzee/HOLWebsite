e24307404c5d6f870dda49915d9bc0bfe13e760a
Brenzee <karelisbrendons@gmail.com>
2020-09-08 10:50:24 +0300

    Deleted a picture from gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Gallery.css";
+
+//All images for gallery
 import Gift from "../../img/img_gallery/gift.jpg";
 import Inside from "../../img/img_gallery/inside.jpg";
-import Inside2 from "../../img/img_gallery/inside_2.jpg";
 import Mojito from "../../img/img_gallery/mojito.jpg";
 import Steak from "../../img/img_gallery/steak.jpg";
 import Wine from "../../img/img_gallery/wine.jpg";
 import Card from "./Card";
 
 const Gallery = () => {
-  const images = [Gift, Inside, Inside2, Mojito, Steak, Wine];
+  const images = [Gift, Inside, Mojito, Steak, Wine];
 
   const arrangeImg = images.map((img) => {
     return <Card src={img} key={img}></Card>;

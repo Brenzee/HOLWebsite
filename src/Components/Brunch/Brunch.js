0ffdc683e6b1f952e734006c1f5140d733ec1be0
Brenzee <karelisbrendons@gmail.com>
2020-10-07 13:56:44 +0300

    Brunch edit

--- src/Components/Brunch/Brunch.js
+++ src/Components/Brunch/Brunch.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./Brunch.css";
+import React from 'react'
+import './Brunch.css'
 
 const Brunch = ({ text }) => {
   return (
-    <div className="brunch about_text" id="brunch">
-      <h2 className="about_title_brunch">{text.brunch_title}</h2>
-      <div className="brunch_about item_text">
+    <div className='brunch' id='brunch'>
+      <h2 className='about_title_brunch'>{text.brunch_title}</h2>
+      <div className='brunch_about item_text'>
         <p>{text.brunch_about}</p>
       </div>
-      <div className="social_media_brunch"></div>
+      <div className='social_media_brunch'></div>
     </div>
-  );
-};
+  )
+}
 
-export default Brunch;
+export default Brunch

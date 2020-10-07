e8ba9dd983821d041d2e7d03685dd918e0978f23
Brenzee <karelisbrendons@gmail.com>
2020-10-07 11:39:29 +0300

    MaterialUi Reservation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
+import { ThemeProvider } from '@material-ui/core/styles'
 
 //Each language page
-import Lv from "./Lv";
-import Eng from "./Eng";
-import Ru from "./Ru";
-import Thanks from "./Components/Thanks/Thanks";
+import Lv from './Lv'
+import Eng from './Eng'
+import Ru from './Ru'
+import Thanks from './Components/Thanks/Thanks'
+import theme from './theme'
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/ru" component={Ru} />
-          <Route path="/eng" component={Eng} />
-          <Route path="/success" component={Thanks} />
-          <Route path="/" exact component={Lv} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-};
+    <>
+      <ThemeProvider theme={theme}>
+        <div>
+          <BrowserRouter>
+            <Switch>
+              <Route path='/ru' component={Ru} />
+              <Route path='/eng' component={Eng} />
+              <Route path='/success' component={Thanks} />
+              <Route path='/' exact component={Lv} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -1,86 +1,117 @@
-import React, { useState } from "react";
-import "./Reservation.css";
-import emailjs from "emailjs-com";
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react'
+import './Reservation.css'
+import emailjs from 'emailjs-com'
+import { Redirect } from 'react-router-dom'
+import { makeStyles, TextField } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
+  },
+}))
 
 function Reservation(props) {
-  const [error, setError] = useState(false);
-  const [done, setDone] = useState(false);
+  const classes = useStyles()
+  const [error, setError] = useState(false)
+  const [done, setDone] = useState(false)
+  const [value, setValue] = useState({
+    name: '',
+    date: '',
+    time: '',
+    count: 0,
+    comments: '',
+  })
+
+  const handleChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    })
+    console.log(value)
+  }
 
   const sendEmail = (e) => {
-    e.preventDefault();
-
+    e.preventDefault()
+    console.log(value)
     emailjs
       .sendForm(
-        "service_ot3o2rd",
-        "template_y074ozf",
-        e.target,
-        "user_7iqekkJfyJwJas5mZO0ww"
+        'service_ot3o2rd',
+        'template_y074ozf',
+        value,
+        'user_7iqekkJfyJwJas5mZO0ww'
       )
       .then(
         (result) => {
-          setDone(true);
+          setDone(true)
         },
         (err) => {
-          setError(true);
+          setError(true)
         }
-      );
-  };
+      )
+  }
 
   return (
-    <div className="reserve" id="reserve">
-      {done && <Redirect to="/success" />}
-      <div className="reserve_title">
+    <div className='reserve' id='reserve'>
+      {done && <Redirect to='/success' />}
+      <div className='reserve_title'>
         <h2>{props.text.reserve_title}</h2>
       </div>
-      <form name="reservation" onSubmit={sendEmail}>
-        <input type="hidden" name="form-name" value="reservation" />
-        <label htmlFor="name">{props.text.reserve_name}</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder={props.text.reserve_name_placeholder}
+      <form name='reservation' onSubmit={sendEmail} className={classes.root}>
+        <input type='hidden' name='form-name' value='reservation' />
+        <TextField
+          variant='outlined'
+          id='name'
+          name='name'
+          onChange={handleChange}
+          label={props.text.reserve_name}
           required
-          title="Lūdzu ievadīt vārdu"
         />
-        <label htmlFor="date">{props.text.reserve_date}</label>
-        <input type="date" id="date" name="date" required />
-        <label htmlFor="time">{props.text.reserve_time}</label>
-        <input type="time" id="time" name="time" required />
-        <label htmlFor="count">{props.text.reserve_count}</label>
-        <input
-          type="number"
-          id="count"
-          name="count"
-          placeholder="2"
-          min="1"
-          max="15"
-          title="Lūdzu ievadīt pareizi viesu skaitu"
+        <TextField
+          variant='outlined'
+          id='date'
+          name='date'
+          type='date'
+          onChange={handleChange}
+          label={props.text.reserve_date}
+          defaultValue='2020-10-01'
+          required
         />
-        <label htmlFor="phone">{props.text.reserve_phone}</label>
-        <div className="phone_number">
-          <div className="phone_plus">+</div>
-          <input
-            type="number"
-            id="phone"
-            name="phone"
-            placeholder=""
-            title="Lūdzu ievadīt pareizi telefona numuru"
-            required
-          />
-        </div>
-        <label htmlFor="comments">{props.text.reserve_comments}</label>
-        <textarea
-          type="text"
-          id="comments"
-          name="comments"
-          placeholder={props.text.reserve_comments_placeholder}
+        <TextField
+          onChange={handleChange}
+          variant='outlined'
+          id='time'
+          name='time'
+          type='time'
+          defaultValue='11:00'
+          label={props.text.reserve_time}
+          required
         />
-        <button type="submit">{props.text.res_button}</button>
+        <TextField
+          onChange={handleChange}
+          variant='outlined'
+          id='count'
+          name='count'
+          type='number'
+          label={props.text.reserve_count}
+          required
+        />
+        <TextField
+          onChange={handleChange}
+          variant='outlined'
+          id='comments'
+          multiline
+          name='comments'
+          rows={4}
+          label={props.text.reserve_comments}
+        />
+
+        <button type='submit'>{props.text.res_button}</button>
         {error && <p>Ir kluda</p>}
       </form>
     </div>
-  );
+  )
 }
-export default Reservation;
+export default Reservation
